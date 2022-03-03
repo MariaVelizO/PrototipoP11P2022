0a573c54edd0e450c13e3fe744c995b8904bc324
María José Véliz <mvelizo1@miumg.edu.gt>
2022-03-03 13:04:22 -0600

    Subiendo PrimerParcial Maria Jose Veliz Ochoa 9959-21-5909

---
+++ PrimerParcial.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int f1(int y, int x)
+{
+return x - y;
+}
+main()
+{
+int x = 3, y = 7;
+int NOMBRE= 0;
+
+printf("%d , %d, %d, %d", x, y, f1(y,x), f1(x,y));
+
+}
+/* NOMBRE: Maria Jose Veliz Ochoa, CARNE: 9959-21-5909" */
